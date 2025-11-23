056487bded00ec2964b265db87d4dbb31fb1732d
alekseytilinin <alexeytilinin@yandex.ru>
2025-11-23 13:09:44 +0300

    Final 2.2

--- Repeat/ViewController/PostViewController.swift
+++ Repeat/ViewController/PostViewController.swift
@@ -8,31 +8,14 @@
 import UIKit
 
 class PostViewController: UIViewController {
-    
-//    private lazy var titleLabale: UILabel = {
-//        let label = UILabel()
-//        label.text = FeedViewController().post.postTitle
-//        label.translatesAutoresizingMaskIntoConstraints = false
-//        return label
-//    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .cyan
         title = FeedViewController().post.postTitle
-//        addConstraints()
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .bookmarks, target: self, action: #selector(showInfoViewController))
     }
-    
-//    private func addConstraints() {
-//        view.addSubview(titleLabale)
-//
-//        NSLayoutConstraint.activate([
-//            titleLabale.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
-//            titleLabale.centerXAnchor.constraint(equalTo: view.centerXAnchor)
-//        ])
-//    }
     
     @objc func showInfoViewController() {
         let infoViewController = InfoViewController()
